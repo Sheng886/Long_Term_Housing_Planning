4da1b76c07b9a1536fd14a6a2d31c4dfc4aca0b9
Sheng886 <shengyc@clemson.edu>
2024-09-03 03:02:18 -0400

    SDDP_Benders

--- MHSP/model/MHSP_SDDP_Benders.py
+++ MHSP/model/MHSP_SDDP_Benders.py
@@ -192,6 +192,7 @@ class StageProblem:
         self.state = state
         self.stage = stage
         self.stage0 = stage0
+        self.last_stage = last_stage
 
         self.cut_rhs = []
         self.cut = []
@@ -282,15 +283,99 @@ class StageProblem:
 
 
 
-        self.model.update()
-        self.model.setParam("OutputFlag", 0)
-        self.model.optimize()
+        # ---------------------------------------- Benders Decompoistion on Strategic Node  -----------------------------------------
 
-        # print("obj:",self.model.ObjVal)
+        LB = 0
+        UB = 1000000
+        eps = 1e-3
+
+
+        while(True):
+            
+            self.model.update()
+            self.model.setParam("OutputFlag", 0)
+            self.model.optimize()
+
+            LB = self.model.ObjVal
+
+            pi_8b = np.zeros((self.args.K, self.args.W, self.args.P))
+            pi_8e = np.zeros((self.args.K, self.args.M+1, self.args.I))
+            pi_8g = np.zeros((self.args.K, self.args.M+1, self.args.W))
+            pi_8h = np.zeros((self.args.K, self.args.M+1, self.args.J, self.args.G))
+            pi_8i = np.zeros((self.args.K, self.args.W, self.args.P))
+
+            sub_opt_total = 0
+
+            for k in range(self.args.K):
+                if(self.stage0 == True):
+                    pi_8b[k],pi_8e[k],pi_8g[k],pi_8h[k],pi_8i[k],sub_opt = self.sub.run(self.u,self.v,self.idata.demand[self.stage][self.state][k],self.idata.J_pro)
+                else:
+                    pi_8b[k],pi_8e[k],pi_8g[k],pi_8h[k],pi_8i[k],sub_opt = self.sub.run(self.u,self.v,self.idata.demand_root[k],self.idata.J_pro)
+                sub_opt_total = sub_opt_total + sub_opt
+
+            sub_opt_total = (1/self.args.K)*sub_opt_total
+
+            # ---------------------------------------- UB,LB Check  -----------------------------------------
+
+            temp_UB = 0
+
+            if(self.last_stage == False):
+                temp_UB = sub_opt_total + sum(self.idata.E_w[w]*self.y[w].x for w in range(self.args.W)) 
+                temp_UB = temp_UB + sum(self.idata.O_p[p]*(self.x[w,p].x - self.idata.R_p[p]*self.z[w,p].x) for w in range(self.args.W) for p in range(self.args.P))
+                temp_UB = temp_UB + sum(self.idata.MC_tran_matrix[self.state][n]*self.theta[n].x for n in range(self.args.N))
+            else:
+                temp_UB = sub_opt_total + sum(self.idata.E_w[w]*self.y[w].x for w in range(self.args.W)) 
+                temp_UB = temp_UB + sum(self.idata.O_p[p]*(self.x[w,p].x - self.idata.R_p[p]*self.z[w,p].x) for w in range(self.args.W) for p in range(self.args.P))
+
+            UB = min(UB,temp_UB)
+
+            if(abs((UB - LB)/UB) <= eps):
+                break
+
+            # ---------------------------------------- Benders Cut  -----------------------------------------
+
+            if(self.phi.x < sub_opt_total - cut_vio_thred and abs(self.phi.x - sub_opt_total)/max(abs(self.phi.x),1e-10) > cut_vio_thred):
+
+                if(self.stage0 == True):
+                    temp_constraint = self.model.addConstr(self.phi >= (1/self.args.K)*quicksum(quicksum(self.v[w,p]*pi_8b[k][w][p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                                      +quicksum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I))
+                                                                                      +quicksum(pi_8g[k][m][w]*self.u[w] for m in range(self.args.M+1) for w in range(self.args.W))
+                                                                                      +quicksum(pi_8h[k][m][j][g]*self.idata.demand[self.stage][self.state][k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G))
+                                                                                      +quicksum(pi_8i[k][w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                                      for k in range(self.args.K)))
+
+                    temp_rhs = (1/self.args.K)*sum(pi_8h[k][m][j][g]*self.idata.demand[self.stage][self.state][k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G) for k in range(self.args.K))
+                    temp_rhs = temp_rhs + (1/self.args.K)*sum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I) for k in range(self.args.K))
+
+                    self.cut.append(temp_constraint)
+                    self.cut_rhs.append(temp_rhs)
+                
+                else:
+                    temp_constraint = self.model.addConstr(self.phi >= (1/self.args.K)*quicksum(quicksum(self.v[w,p]*pi_8b[k][w][p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                                      +quicksum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I))
+                                                                                      +quicksum(pi_8g[k][m][w]*self.u[w] for m in range(self.args.M+1) for w in range(self.args.W))
+                                                                                      +quicksum(pi_8h[k][m][j][g]*self.idata.demand_root[k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G))
+                                                                                      +quicksum(pi_8i[k][w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                                      for k in range(self.args.K)))
+
+                    temp_rhs = (1/self.args.K)*sum(pi_8h[k][m][j][g]*self.idata.demand_root[k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G) for k in range(self.args.K))
+                    temp_rhs = temp_rhs + (1/self.args.K)*sum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I) for k in range(self.args.K))
+               
+                    self.cut.append(temp_constraint)
+                    self.cut_rhs.append(temp_rhs)
+
+
+                # print(self.phi.x, sub_opt_total)
+                # print("stage:",self.stage,"state:",self.state,"add Benders cut")
+
+
+           
+
+            
 
         return self.u,self.v
 
-    def backward_run(self):
+    def backward_run(self,iter):
 
 
         # ---------------------------------------- Get Dual -----------------------------------------
@@ -313,64 +398,26 @@ class StageProblem:
                     pi_c[w][p] = self.c_inv_level[w][p].pi
                     temp = temp + self.c_inv_level[w][p].pi*self.v_pre[w,p].x
 
-        cut_pi = 0
+        Benders_cut_pi = 0
 
         # Cut
         if(self.cut):
             for c in range(len(self.cut_rhs)):
                 temp = temp + self.cut[c].pi*self.cut_rhs[c]
-                cut_pi = cut_pi + self.cut[c].pi*self.cut_rhs[c] 
+                Benders_cut_pi = Benders_cut_pi + self.cut[c].pi*self.cut_rhs[c]
+                # print(self.stage,self.state,c)
 
 
         if(abs(temp-self.model.ObjVal) >= 1e-5):
-            print("stage:",self.stage,"problematic dual solution!")
+            print("iteration:",iter,"stage:",self.stage,"problematic dual solution!")
             print("temp:",temp)
             print("obj:",self.model.ObjVal)
             pdb.set_trace()
 
-        # ---------------------------------------- Add Benders Cut -----------------------------------------
-        
-        sub_opt_total = 0
 
-        pi_8b = np.zeros((self.args.K, self.args.W, self.args.P))
-        pi_8e = np.zeros((self.args.K, self.args.M+1, self.args.I))
-        pi_8g = np.zeros((self.args.K, self.args.M+1, self.args.W))
-        pi_8h = np.zeros((self.args.K, self.args.M+1, self.args.J, self.args.G))
-        pi_8i = np.zeros((self.args.K, self.args.W, self.args.P))
+        return pi_b,pi_c,Benders_cut_pi,self.model.ObjVal
 
-
-        for k in range(self.args.K):
-            if(self.stage0 == True):
-                pi_8b[k],pi_8e[k],pi_8g[k],pi_8h[k],pi_8i[k],sub_opt = self.sub.run(self.u,self.v,self.idata.demand[self.stage][self.state][k],self.idata.J_pro)
-            else:
-                pi_8b[k],pi_8e[k],pi_8g[k],pi_8h[k],pi_8i[k],sub_opt = self.sub.run(self.u,self.v,self.idata.demand_root[k],self.idata.J_pro)
-            sub_opt_total = sub_opt_total + sub_opt
-
-        sub_opt_total = (1/self.args.K)*sub_opt_total
-
-        if(self.phi.x < sub_opt_total - cut_vio_thred and abs(self.phi.x - sub_opt_total)/max(abs(self.phi.x),1e-10) > cut_vio_thred):
-
-            temp_constraint = self.model.addConstr(self.phi >= (1/self.args.K)*quicksum(quicksum(self.v[w,p]*pi_8b[k][w][p] for w in range(self.args.W) for p in range(self.args.P))
-                                                                                  +quicksum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I))
-                                                                                  +quicksum(pi_8g[k][m][w]*self.u[w] for m in range(self.args.M+1) for w in range(self.args.W))
-                                                                                  +quicksum(pi_8h[k][m][j][g]*self.idata.demand[self.stage][self.state][k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G))
-                                                                                  +quicksum(pi_8i[k][w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P))
-                                                                                  for k in range(self.args.K)))
-            
-            temp_rhs = (1/self.args.K)*sum(pi_8h[k][m][j][g]*self.idata.demand[self.stage][self.state][k][g][m]*self.idata.J_pro[j] for m in range(1,self.args.M+1) for j in range(self.args.J) for g in range(self.args.G) for k in range(self.args.K))
-            temp_rhs = temp_rhs + (1/self.args.K)*sum(self.idata.B_i[i]*pi_8e[k][m][i] for m in range(self.args.M+1) for i in range(self.args.I) for k in range(self.args.K))
-
-            self.cut_temp.append(temp_constraint)
-            self.cut_rhs_temp.append(temp_rhs)
-
-            print("stage:",self.stage,"state:",self.state,"add cut")
-
-
-
-
-        return pi_b,pi_c,self.model.ObjVal
-
-    def add_cut(self,obj,stage_next,state_sample_path,state_next,pi_b,pi_c):
+    def add_cut(self,obj,stage_next,state_sample_path,state_next,pi_b,pi_c,Benders_cut_pi):
 
         temp_cut_itr = 0
 
@@ -379,13 +426,28 @@ class StageProblem:
             if(self.theta[state_next].x < obj - cut_vio_thred and abs(self.theta[state_next].x - obj)/max(abs(self.theta[state_next].x),1e-10) > cut_vio_thred):
 
                 temp_constraint = self.model.addConstr(self.theta[state_next] >= quicksum(pi_b[w]*self.u[w] for w in range(self.args.W)) 
-                                                                                + quicksum(pi_c[w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P)))
+                                                                                + quicksum(pi_c[w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                                + Benders_cut_pi)
+
+                temp_rhs = Benders_cut_pi
+        
+                self.cut_temp.append(temp_constraint)
+                self.cut_rhs_temp.append(temp_rhs)
+
                 temp_cut_itr = 1
+                # print("stage:",self.stage,"state:",self.state,"SDDP Cut")
             else:
                 temp_cut_itr = 0
         else:
             temp_constraint = self.model.addConstr(self.theta[state_next] >= quicksum(pi_b[w]*self.u[w] for w in range(self.args.W)) 
-                                                                + quicksum(pi_c[w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P)))
+                                                                + quicksum(pi_c[w][p]*self.v[w,p] for w in range(self.args.W) for p in range(self.args.P))
+                                                                + Benders_cut_pi)
+            temp_rhs = Benders_cut_pi
+        
+            self.cut_temp.append(temp_constraint)
+            self.cut_rhs_temp.append(temp_rhs)
+
+            # print("stage:",self.stage,"state:",self.state,"SDDP Cut")
 
         return temp_cut_itr
 
@@ -446,6 +508,7 @@ class solve_SDDP:
 
             # sample path
             sample_path = self.sample_path(self.args)
+            # print(sample_path)
 
             # ---------------------------------------------------- Forward ----------------------------------------------------
             u,v = self.stage_root.forward_run()
@@ -455,20 +518,20 @@ class solve_SDDP:
 
 
             # ----------------------------------- Backward -----------------------------------
-            pi_b,pi_c,LB = self.stage_leaf[sample_path[self.args.T-1]].backward_run()
+            pi_b,pi_c,Benders_cut_pi,LB = self.stage_leaf[sample_path[self.args.T-1]].backward_run(iter)
             for stage in reversed(range(self.args.T-1)):
 
                 # ---------------------------- Cut Sharing ----------------------------
                 for state in range(self.args.N):
-                    cut_iter_temp = self.stage[stage][state].add_cut(LB,stage+1,sample_path[stage],sample_path[stage+1],pi_b,pi_c)
+                    cut_iter_temp = self.stage[stage][state].add_cut(LB,stage+1,sample_path[stage],sample_path[stage+1],pi_b,pi_c,Benders_cut_pi)
                     cutviol_iter = cutviol_iter + cut_iter_temp
 
-                pi_b,pi_c,LB =  self.stage[stage][sample_path[stage]].backward_run()
+                pi_b,pi_c,Benders_cut_pi,LB =  self.stage[stage][sample_path[stage]].backward_run(iter)
 
-            cut_iter_temp = self.stage_root.add_cut(LB,0,0,sample_path[0],pi_b,pi_c)
+            cut_iter_temp = self.stage_root.add_cut(LB,0,0,sample_path[0],pi_b,pi_c,Benders_cut_pi)
             cutviol_iter = cutviol_iter + cut_iter_temp
 
-            pi_b,pi_c,LB =  self.stage_root.backward_run()
+            pi_b,pi_c,LB,Benders_cut_pi =  self.stage_root.backward_run(iter)
 
             LB_list.append(LB)
             if(iter%50 == 0):
@@ -480,6 +543,7 @@ class solve_SDDP:
             if flag != 0:
                 train_time = Elapsed
                 print("total time:", Elapsed)
+                print("Best Lower Bound:", LB_list[iter])
                 break
 
 
